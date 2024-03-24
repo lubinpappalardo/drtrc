385cda7204c14fce3b6cd836ceb98424eb1f6356
Lubin Pappalardo <lubinpappalardo@gmail.com>
2024-03-24 16:02:35 -0400

    responsive design

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ document.getElementById("follow-popup-close-btn").addEventListener("click", func
     document.getElementById("follow-popup").classList = "";
 });
 
-document.getElementById("follow-us-btn").addEventListener("click", function() {
-    document.getElementById("follow-popup").classList = "visible";
+const followUsBtns = document.getElementsByClassName("follow-us-btn");
+Array.from(followUsBtns).forEach(element => {
+    element.addEventListener("click", function() {
+        document.getElementById("follow-popup").classList = "visible";
+        document.getElementById("nav").classList = "";
+    });
+});
+
+
+function isMobile() {
+    return window.innerWidth < 768;
+}
+
+document.getElementById("mobile-menu").addEventListener("click", function() {
+    if (isMobile()) {
+        document.getElementById("nav").classList = "visible";
+    }
+});
+
+document.getElementById("mobile-nav-close-btn").addEventListener("click", function() {
+        document.getElementById("nav").classList = "";
 });
